d3a859138334259ffa2a031f9d22b8d73daee38c
Wrdouuu <164611150+Wrdouuu@users.noreply.github.com>
2024-08-19 21:03:25 +0700

    S

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GIATRITRONGMANG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+
+            Console.WriteLine("Enter student name: ");
+            string name = Console.ReadLine();
+
+            bool checkName=false;
+
+
+            for (int i=0; i <= students.Length;i++)
+            {
+                if (students[i].Equals(name))
+                {
+                    Console.WriteLine("Position of the students in the list " + name + " is: " + (i + 1));
+                    break;
+                }
+                if (checkName=false)
+                {
+                    Console.WriteLine("Name not exist");
+                }
+                
+            }
+            
+        }
+    }
+}
